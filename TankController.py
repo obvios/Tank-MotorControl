0d762d48b3b4e56ddbea5661913d76339356dcdc
Eric Palma <palmae95@gmail.com>
2022-07-10 19:28:22 -0400

    init basic definition of TankController

--- TankController.py
+++ TankController.py
@@ -1,2 +1,13 @@
 # Module to abstract upon MotorController.py to control the movement of the tank
 from MotorController import MotorController
+
+class TankController:
+    
+    def __init__(self) -> None:
+        self.motorController = MotorController()
+
+    def setup(self):
+        self.motorController.setup()
+
+    def cleanup(self):
+        self.motorController.cleanup()
