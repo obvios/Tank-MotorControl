355775a5127b0a712469bf0052bcb59a3495c811
Eric Palma <palmae95@gmail.com>
2022-07-17 14:57:33 -0400

    implement run method

--- TankControllerPart.py
+++ TankControllerPart.py
@@ -4,6 +4,25 @@ class TankControllerPart:
 
     def __init__(self, controller: TankController) -> None:
         self.tankController = controller
+        self.tankController.motorsOn()
 
     def run(self, moving_forward, moving_reverse, steering_left, steering_right):
-        pass
+        if moving_forward:
+            self.tankController.moveForward()
+            if steering_left:
+                self.tankController.turnLeft()
+            elif steering_right:
+                self.tankController.turnRight
+        elif moving_reverse:
+            self.tankController.moveBackwards()
+            if steering_left:
+                self.tankController.turnLeft()
+            elif steering_right:
+                self.tankController.turnRight()
+        elif steering_left:
+            self.tankController.spinLeft()
+        elif steering_right:
+            self.tankController.spinRight()
+        else:
+            self.tankController.stop()
+            
