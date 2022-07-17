8f55fb6c21f4f2fd258bd584e540db0482ec5bf3
Eric Palma <epalmaa714@gmail.com>
2022-07-16 20:55:33 -0400

    renaming

--- PS4ControllerPart.py
+++ PS4ControllerPart.py
@@ -4,48 +4,48 @@ class PS4ControllerPart(Controller):
 
     def __init__(self, **kwargs):
         Controller.__init__(self, **kwargs)
-        self.throttlePressed = False
-        self.reversePressed = False
-        self.steeringLeft = False
-        self.steeringRight = False
+        self.x_pressed = False
+        self.square_pressed = False
+        self.left_arrow_pressed = False
+        self.right_arrow_pressed = False
 
     def on_x_press(self):
         print("on_x_press")
-        self.throttlePressed = True
+        self.x_pressed = True
 
     def on_x_release(self):
         print("on_x_release")
-        self.throttlePressed = False
+        self.x_pressed = False
 
     def on_square_press(self):
         print("on_square_press")
-        self.reversePressed = True
+        self.square_pressed = True
         
     def on_square_release(self):
         print("on_square_release")
-        self.reversePressed = False
+        self.square_pressed = False
 
     def on_left_arrow_press(self):
         print("on_left_arrow_press")
-        self.steeringLeft = True
+        self.left_arrow_pressed = True
 
     def on_right_arrow_press(self):
         print("on_right_arrow_press")
-        self.steeringRight = True
+        self.right_arrow_pressed = True
 
     def on_left_right_arrow_release(self):
         print("on_left_right_arrow_release")
-        self.steeringLeft = False
-        self.steeringRight = False
+        self.left_arrow_pressed = False
+        self.right_arrow_pressed = False
 
     def update(self):
         print("starting to listen to ps4 controller events")
         self.listen() 
         
     def run_threaded(self):
-        """:return: throttlePressed, reversePressed, steeringLeft, steeringRight"""
+        """:return: x_pressed, square_pressed, left_arrow_pressed, right_arrow_pressed"""
         print("PS4ControllerPart.run_threaded")
-        return self.throttlePressed, self.reversePressed, self.steeringLeft, self.steering.right
+        return self.x_pressed, self.square_pressed, self.left_arrow_pressed, self.right_arrow_pressed
 
 
 # ONLY FOR TESTING
