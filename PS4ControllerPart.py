834aae432fc03d4a3f006bb343e5c59500d8fd1b
Eric Palma <epalmaa714@gmail.com>
2022-07-16 20:01:54 -0400

    defining overriden controller for ps4

--- PS4ControllerPart.py
+++ PS4ControllerPart.py
@@ -1,7 +1,16 @@
 from pyPS4Controller.controller import Controller
 
+class PS4ControllerPart(Controller):
+
+    def __init__(self, **kwargs):
+        Controller.__init__(self, **kwargs)
+
+    def on_x_press(self):
+        print("on_x_press")
+
+
 class TestController(Controller):
-  def __ini__(self, **kwargs):
+  def __init__(self, **kwargs):
     Controller.__init__(self,**kwargs)
 
 controller = TestController(interface="/dev/input/js0",connecting_using_ds4drv=False)
